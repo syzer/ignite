1bb30a092c6b3db01c83ec30f5419dd735456e99
vozerov-gridgain <vozerov@gridgain.com>
2016-05-31 09:11:25 +0300

    Possible fix.

--- modules/hadoop/src/main/java/org/apache/ignite/hadoop/fs/CachingHadoopFileSystemFactory.java
+++ modules/hadoop/src/main/java/org/apache/ignite/hadoop/fs/CachingHadoopFileSystemFactory.java
@@ -47,7 +47,7 @@ public class CachingHadoopFileSystemFactory extends BasicHadoopFileSystemFactory
     private final transient HadoopLazyConcurrentMap<String, FileSystem> cache = new HadoopLazyConcurrentMap<>(
         new HadoopLazyConcurrentMap.ValueFactory<String, FileSystem>() {
             @Override public FileSystem createValue(String key) throws IOException {
-                return getWithMappedName(key);
+                return CachingHadoopFileSystemFactory.super.getWithMappedName(key);
             }
         }
     );
@@ -61,8 +61,7 @@ public class CachingHadoopFileSystemFactory extends BasicHadoopFileSystemFactory
 
     /** {@inheritDoc} */
     @Override public FileSystem getWithMappedName(String name) throws IOException {
-//        return cache.getOrCreate(name);
-        return cache.getOrCreate(IgfsUtils.fixUserName(name));
+        return cache.getOrCreate(name);
     }
 
     /** {@inheritDoc} */
